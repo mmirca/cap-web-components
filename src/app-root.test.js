96238ebf34a07698aadb772ed9b54e384a284f1b
mmirca <marius.mirca@gmail.com>
2020-04-28 19:50:40 +0200

    docs(app-root.test): add hints about the test scaffolding

--- src/app-root.test.js
+++ src/app-root.test.js
@@ -2,8 +2,20 @@ import { AppRoot } from './app-root';
 
 describe('AppRoot', () => {
 
+  /**
+   * We initialize a variable where we store the reference to the component we are
+   * testing for each case
+   */
   let fixture;
 
+  /**
+   * Jest unit tests are powered by a virtual DOM using JSDOM (https://github.com/jsdom/jsdom)
+   * Before running each case we need to setup the virtual DOM by attaching our component and
+   * waiting for it to finish updating (https://lit-element.polymer-project.org/guide/lifecycle#updatecomplete)
+   * 
+   * To test events you can use the provided dispatchEvent stub, for more information about stubs
+   * you can check the jest mock functions docs (https://jestjs.io/docs/en/mock-functions.html)
+   */
   beforeEach(async () => {
     fixture = new AppRoot();
     document.body.appendChild(fixture);
@@ -11,6 +23,10 @@ describe('AppRoot', () => {
     fixture.dispatchEvent = jest.fn();
   });
 
+  /**
+   * For better tests performance it is recommended to keep a lean virtual DOM, in order to
+   * accomplish this we remove the used component instance after each case
+   */
   afterEach(() => {
     document.body.removeChild(fixture);
   });
